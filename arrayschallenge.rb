46ab30d18eeaefe8551014049b097e2106cbf782
Your Name <you@example.com>
2018-08-27 10:13:58 +1000

    Arrays challenge complete.

---
+++ arrayschallenge.rb
@@ -0,0 +1,11 @@
+arr = ['Calf', 'Duck', 'Elephant', 'Goat', 'Lamb', 'Lion', 'Mule', 'Dog']
+arr.push('Puma')
+arr.insert(4, 'Joey')
+arr.delete('Dog')
+arr.reverse!
+arr.delete('Elephant')
+arr.insert(6, 'Foal')
+arr.push('Bear')
+arr.reverse!
+puts arr
+
