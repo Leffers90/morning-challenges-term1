ae72e9ecd78d5aee67706b92dd7062ec4eb83589
Your Name <you@example.com>
2018-09-03 09:43:15 +1000

    resolve conflicts

--- 07_fib_seq_01.rb
+++ 07_fib_seq_01.rb
@@ -1,6 +1,8 @@
 # A slightly different kind of challenge this morning.
 
-# The Company has broken the keyboard on their one and only laptop, so can’t get this bit of code to run. What would the output of this code be (without running it on your own computer)? 
+# The Company has broken the keyboard on their one and only laptop, 
+# so can’t get this bit of code to run. What would the output of this code be 
+# (without running it on your own computer)? 
 
 #  def fib(num_
 #   if num < 2
@@ -10,3 +12,12 @@
 #   end
 # end
 # puts fib(4)
+
+def fib(num)
+    if num < 2
+       num
+    else
+        fib(num-1) + fib(num-2)
+    end
+end
+puts fib(4) 
